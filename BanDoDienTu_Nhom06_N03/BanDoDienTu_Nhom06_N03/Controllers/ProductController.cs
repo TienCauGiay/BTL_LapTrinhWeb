5cf60f53470586612ab66a8377ac6ad4f815c6b1
Anh Tien Dep Trai <tiensd17052001@example.com>
2023-03-23 16:27:50 +0700

    tim kiem duoc theo danh muc r nhung chua load duoc anh, k biet bi sao

---
+++ BanDoDienTu_Nhom06_N03/BanDoDienTu_Nhom06_N03/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using BanDoDienTu_Nhom06_N03.Models;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace BanDoDienTu_Nhom06_N03.Controllers
+{
+    public class ProductController : Controller
+    {
+        BanDoDienTuContext _context = null;
+
+        public ProductController(BanDoDienTuContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ProductByCategory(string id, int? page)
+        {
+            int pageSize = 16;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var products = _context.SanPhams.Where(x => x.MaDm == id).ToList();
+            PagedList<SanPham> res = new PagedList<SanPham>(products, pageNumber, pageSize);
+            ViewBag.MaDm = id;
+            return View(res);
+        }
+    }
+}
