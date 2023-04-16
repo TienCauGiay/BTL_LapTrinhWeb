56991c2993e893736d2c369d8d191bf878c90b96
Anh Tien Dep Trai <tiensd17052001@example.com>
2023-04-16 12:35:55 +0700

    update file sql

---
+++ BanDoDienTu_Nhom06_N03/BanDoDienTu_Nhom06_N03/Areas/Admin/Controllers/DoanhThuController.cs
@@ -0,0 +1,32 @@
+﻿using BanDoDienTu_Nhom06_N03.Models;
+using BanDoDienTu_Nhom06_N03.Models.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanDoDienTu_Nhom06_N03.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authentication]
+    public class DoanhThuController : Controller
+    {
+        BanDoDienTuContext _context = null;
+        public DoanhThuController()
+        {
+            _context = new BanDoDienTuContext();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TinhDoanhThu(int month, int year)
+        {
+            var doanhThu = _context.HoaDonBans
+                .Where(hdb => hdb.NgayBan.Value.Month == month && hdb.NgayBan.Value.Year == year)
+                .Sum(hdb => hdb.TongTien);
+
+            ViewBag.DoanhThu = doanhThu;
+
+            return View();
+        }
+    }
+}
